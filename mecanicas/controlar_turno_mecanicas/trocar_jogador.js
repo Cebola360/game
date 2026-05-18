32d7f4a718e1103074a1b980a1d908baaef48ef1
Simons2003 <lucaslsar2003@gmail.com>
2026-05-18 14:09:29 -0300

    Arrumando o box ativo que tinha quebrado e adicionando a cor do jogador no timer.

--- mecanicas/controlar_turno_mecanicas/trocar_jogador.js
+++ mecanicas/controlar_turno_mecanicas/trocar_jogador.js
@@ -1,11 +1,17 @@
+import { atualizarJogadorAtivo } from "./atualizar_jogador_ativos.js";
+
 export function trocarJogador(estado) {
   const jogador1 = estado.jogadores[0];
   const jogador2 = estado.jogadores[1];
 
+  const timer = document.querySelector(".timer");
+
   if (estado.jogadorAtual === jogador1) {
     estado.jogadorAtual = jogador2;
+    timer.style.backgroundColor ="blue";
   } else {
     estado.jogadorAtual = jogador1;
+    timer.style.backgroundColor ="red";
   }
 
   atualizarJogadorAtivo(estado);
